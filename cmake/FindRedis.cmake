76b352ff1999affbe70361626d42154bb6584883
Unknown <likdudi@gmail.com>
2017-08-02 13:16:01 -0700

    Adding support to find redis package
    Adding support to find redis package

---
+++ cmake/FindRedis.cmake
@@ -0,0 +1,10 @@
+find_library(REDIS_LIBRARY NAMES redis)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(redis REQUIRED_VARS REDIS_LIBRARY)
+
+if(REDIS_FOUND)
+    message(STATUS "Found libraries - ${REDIS_LIBRARY}")
+else(REDIS_FOUND)
+    message(FATAL_ERROR "Redis libraries not found")
+endif(REDIS_FOUND)
