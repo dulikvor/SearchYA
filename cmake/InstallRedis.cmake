ad2201684142f4ec4c360fe4f52bd2efcc4104d4
Unknown <likdudi@gmail.com>
2017-08-03 01:12:43 -0700

    Setting redis installation location
    Setting redis installation location

--- cmake/InstallRedis.cmake
+++ cmake/InstallRedis.cmake
@@ -4,21 +4,18 @@ if (NOT REDIS_FOUND)
             DOWNLOAD_NAME       redis-4.0.0.tar.gz
             URL                 https://github.com/antirez/redis/archive/4.0.0.tar.gz
             CONFIGURE_COMMAND   ""
-            BUILD_COMMAND       cd <SOURCE_DIR> && make
-            INSTALL_COMMAND     ""
+            BUILD_COMMAND       make -C <SOURCE_DIR>
+            INSTALL_COMMAND     make -C <SOURCE_DIR> PREFIX=<INSTALL_DIR> install
             TEST_COMMAND        ""
             )
 
     ExternalProject_Get_Property(Redis INSTALL_DIR)
 
-#    set (GLOG_ROOT_DIR          ${INSTALL_DIR})
-#   set (GLOG_INCLUDE_PATH      ${GLOG_ROOT_DIR}/include)
-#    set (GLOG_LIBRARY           ${GLOG_ROOT_DIR}/lib/libglog.a)
-#    set (GLOG_FOUND             YES)
+    set (REDIS_ROOT_DIR          ${INSTALL_DIR})
+    set (REDIS_FOUND             YES)
 
 #    add_library(GLOG_LIBRARY STATIC IMPORTED)
 #    add_dependencies(GLOG_LIBRARY GLog)
 #    mark_as_advanced(GLOG_LIBRARY GLOG_INCLUDE_PATH)
 
-#    message(STATUS "Use bundled glog v${GLOG_VERSION}")
 endif ()
