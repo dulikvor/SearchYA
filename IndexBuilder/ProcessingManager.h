901e84267065661d38365964f49caaf946b7eba5
Unknown <likdudi@gmail.com>
2017-10-09 13:12:34 -0700

    Removal of unnecessary files.

--- IndexBuilder/ProcessingManager.h
+++ IndexBuilder/ProcessingManager.h
@@ -30,7 +30,6 @@ public:
 private:
 	void RunNewTask(Task* task);
 	void OnTaskCompletion(Task* task);
-	std::shared_ptr<core::AsyncTask> CreateAsyncTask(TaskType taskType, std::unique_ptr<Task> task);
 
 private:
 	mutable std::mutex m_completionMut;
